a13283eb8654ff18654729b8c3672293a72f617c
GRAZIELE MOTA DA SILVA <021.898684@senaicimatec.edu.br>
2024-09-23 18:08:46 -0300

    SEGUNDO COMMIT

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>ViVENCIALY</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+
+    <div class="banner">
+    <img src="caminho.png">
+    </div>
+
+    <div class="text">
+    <h1>Bem-vindo à Loja de Joias</h1>
+    <p>Explore nossas coleções exclusivas.</p>
+    </div>
+
+</body>
+</html>
